fa35c8806daa1b369dea500bc4816245e7944808
Kai-Qiao <joe.mikey.k@gmail.com>
2021-11-03 14:40:55 +0800

    update headless

--- protractor.config.js
+++ protractor.config.js
@@ -76,7 +76,7 @@ exports.config = {
         "ignore-certificate-errors",
         "ignore-ssl-errors",
         "no-sandbox",
-        // "headless",
+        "headless",
       ],
       prefs: {
         download: {
